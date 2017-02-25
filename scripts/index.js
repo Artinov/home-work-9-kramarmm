dba1dc74b6227956a0f1773bb2d892a5316805f7
kramarmm <kramar.mariya@mail.ru>
2017-02-25 12:48:20 +0200

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 var ball = document.querySelector(".discoball");
 
+var ballClasses = ["here", "goes", "our", "awesome", "disco", "ball"];
+i = ballClasses.length -1;
 function goDisco() {
-	//do your disco magic here !
+	ball.classList.remove(ballClasses[i+1]);
+	ball.classList.add(ballClasses[i]);
+	if(i>0){
+	return --i;
+	}else {
+		return i = 5;
+	}
 }
 
 setInterval(goDisco,300);
